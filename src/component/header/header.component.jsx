beb0e5d10c7b51d06727e0c4602fd498308749f6
Himanshu Seth <hseth9897@gmail.com>
2021-01-07 21:03:05 +0530

    first commit

---
+++ src/component/header/header.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { link } from 'react-router-dom';
+
+import './header.style.scss';
+
+const Header = () => (
+    <div className='header'>
+        <link to="/" >
+        
+        </link>
+    </div>
+);
